f78cb83071299e61259f1f5da0276529a6424082
Lucas Myers <myerslucas@Lucas-iMac.local>
2017-03-15 11:49:30 -0700

    Updated PWM generator and connect all modules.

--- verilog_src/flight_controller.v
+++ verilog_src/flight_controller.v
@@ -10,7 +10,7 @@ input wire yaw_pwm;
 input wire throttle_pwm;
 
 wire clk;
-wire w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12;
+wire w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15, w16, w17, w18, w19, w20, w21, w22, w23, w24;
 
 clock clock_inst(clk);
 
@@ -19,16 +19,21 @@ reciever_reader roll_reader(w2, roll_pwm, clk);
 reciever_reader yaw_reader(w3, yaw_pwm, clk);
 reciever_reader throttle_reader(w4, throttle_pwm, clk);
 
+/*output the offset for the respective control to motor 1 on the first wire, motor 2 on the second wire
+, motor 3 on the third wire, and motor 4 on the fourth wire.*/
 pitch_offset_generator pitch_offset_generator_inst(w5, w6, w7, w8, w1, clk);
-roll_offset_generator roll_offset_generator_inst(w5, w6, w7, w8, w2, clk);
-yaw_offset_generator yaw_offset_generator_inst(w5, w6, w7, w8, w3, clk);
-throttle_offset_generator throttle_offset_generator_inst(w5, w6, w7, w8, w4, clk);
+roll_offset_generator roll_offset_generator_inst(w9, w10, w11, w12, w2, clk);
+yaw_offset_generator yaw_offset_generator_inst(w13, w14, w15, w16, w3, clk);
+throttle_offset_generator throttle_offset_generator_inst(w17, w18, w19, w20, w4, clk);
 
-//how do the summers connect now?
+motor_offset_summer motor_1_offset_summer(w21/*output offset of motor 1 to w21*/, w5, w9, w13, w17, clk);
+motor_offset_summer motor_2_offset_summer(w22/*output offset of motor 2 to w22*/, w6, w10, w14, w18, clk);
+motor_offset_summer motor_3_offset_summer(w23/*output offset of motor 3 to w23*/, w7, w11, w15, w19, clk);
+motor_offset_summer motor_4_offset_summer(w24/*output offset of motor 3 to w24*/, w8, w12, w16, w20, clk);
 
-pwm_generator motor1_pwm_generator(motor1_pwm, clk, 8'b01111111);
-pwm_generator motor2_pwm_generator(motor2_pwm, clk, 8'b00000001);
-pwm_generator motor3_pwm_generator(motor3_pwm, clk, 8'b00000011);
-pwm_generator motor4_pwm_generator(motor4_pwm, clk, 8'b00001111);
+pwm_generator motor1_pwm_generator(motor1_pwm, w21, clk);
+pwm_generator motor2_pwm_generator(motor2_pwm, w22, clk);
+pwm_generator motor3_pwm_generator(motor3_pwm, w23, clk);
+pwm_generator motor4_pwm_generator(motor4_pwm, w24, clk);
 
 endmodule
--- verilog_src/pwm_generator.v
+++ verilog_src/pwm_generator.v
@@ -1,24 +1,32 @@
 // PWM generator to output a PWM signal. 
-module pwm_generator (pwm_out, clk, offset);
+module pwm_generator (pwm_out, offset, clk);
 	
 	// Define module inputs and outputs.
 	input wire clk;
-	input [7:0] offset;
+	input offset;
 	output reg pwm_out;
 	
-	reg [7:0] counter = 8'b00000000;
+	integer temp = 0;
+	integer temp2 = 0;
+	integer counter = 0;
 	
 	// Generate the PWM signal based on the offset for duty cycle.
 	always @ (posedge clk)
 	begin
+			temp <= temp + 1;
 			if (offset > counter)
 				pwm_out <= 0;
 			else 
 				pwm_out <= 1;
-
-			counter <= counter + 1'b1;
-			if (counter >= 8'b11111111)
-				counter <= 8'b00000000;
+			assign temp2 = temp % 532;//this will increment every 100th of a second
+			if( temp2 == 0)
+				counter <= counter + 1;
+			if (counter >= 100)
+			begin
+				counter <= 0;
+				temp <= 0;
+				temp2 = 0;
+			end
 	end
 		
 endmodule			
